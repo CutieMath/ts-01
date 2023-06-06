b615942fac03aa0035ab8effcaf9e4dd4fedf925
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-06 14:34:08 +1000

    declare arrays

--- myArray.ts
+++ myArray.ts
@@ -1,1 +1,24 @@
-const superHeros = []
+// declare
+const superHeros: string[] = []
+const superPowerRating: number[] = []
+const superPowerRatingWithAltDeclaration: Array<number> = []
+superHeros.push('Iron Man')
+superPowerRating.push(100)
+
+// declare object type
+type SuperUser = {
+    name: string,
+    powerRating: number
+}
+const allSuperUsers: SuperUser[] = []
+allSuperUsers.push({
+    name: 'Iron Man',
+    powerRating: 100
+})
+
+// declare multidimensional array
+const MLModels: number[][] = [
+    [255, 255, 255]
+]
+MLModels.push([1,2,3])
+// MLModels.push("test") // this will give an error
